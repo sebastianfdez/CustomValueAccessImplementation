e4df9d3315b7fc1dd796b2725bb5f2494f7bdbd5
Sebastian <sebastian.fernandez@hardis.fr>
2019-02-07 16:26:02 +0100

    Polyfills for stackblitz

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -40,6 +40,7 @@
  * https://developers.google.com/search/docs/guides/rendering
  **/
 // import 'core-js/es6/array';
+import 'core-js/es7/reflect';
 
 /** IE10 and IE11 requires the following for NgClass support on SVG elements */
 // import 'classlist.js';  // Run `npm install --save classlist.js`.
@@ -73,7 +74,6 @@
  * Zone JS is required by default for Angular itself.
  */
 import 'zone.js/dist/zone';  // Included with Angular CLI.
-import 'core-js/es7/reflect';
 
 
 /***************************************************************************************************
